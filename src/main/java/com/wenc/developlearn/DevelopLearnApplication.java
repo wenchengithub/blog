7851a64894347c129a5631ba15e03082863801b7
wenchen <wenchen_email@163.com>
2020-10-02 11:27:15 +0800

    init

---
+++ src/main/java/com/wenc/developlearn/DevelopLearnApplication.java
@@ -0,0 +1,13 @@
+package com.wenc.developlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevelopLearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wenc/developlearn/DevelopLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wenc.developlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
